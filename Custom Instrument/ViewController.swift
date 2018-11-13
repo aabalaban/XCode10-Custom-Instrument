31f55bd5cb34b3a7929d09c9f601e5e3208cb28c
Alexander <alexander.balaban@corp.badoo.com>
2018-11-13 11:28:53 +0000

    Initial Commit

---
+++ Custom Instrument/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Custom Instrument
+//
+//  Created by Alexander Balaban on 13/11/2018.
+//  Copyright © 2018 Alexander Balaban. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
